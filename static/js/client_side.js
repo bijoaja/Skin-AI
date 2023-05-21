ab7f76afc0b8e67716a0b7f3b43d8061ba9612e2
Wisnu Wijaya <61434712+Wisnuoke34@users.noreply.github.com>
2023-05-21 12:33:45 +0700

    Update client_side.js

--- static/js/client_side.js
+++ static/js/client_side.js
@@ -1,5 +1,59 @@
 $(document).ready(function(){
 
+   // Fungsi untuk memeriksa apakah ada gambar yang diunggah
+   function checkImageUploaded() {
+    var fileSelected = $('#inputImage').prop('files').length > 0;
+    $("#prediksi_submit").prop("disabled", !fileSelected);
+  }
+
+  // Memanggil fungsi checkImageUploaded saat ada perubahan pada unggahan gambar
+  $('#inputImage').on("change", function() {
+    checkImageUploaded();
+  });
+
+  // Memeriksa status unggahan gambar saat halaman dimuat
+  checkImageUploaded();
+
+  // Fungsi untuk memanggil API ketika tombol prediksi ditekan
+  $("#prediksi_submit").click(function(e) {
+    e.preventDefault();
+
+    // Cek kembali apakah ada gambar yang diunggah
+    var fileSelected = $('#inputImage').prop('files').length > 0;
+    if (!fileSelected) {
+      alert("Please upload images for analysis!");
+      return;
+    }
+
+  });
+
+    // Fungsi untuk memeriksa apakah ada skin tone yang dipilih
+  function checkSkinToneSelected() {
+    var selectedValue = $('#skinToneSelect').val();
+    $("#prediksi_submit").prop("disabled", !selectedValue);
+  }
+
+  // Memanggil fungsi checkSkinToneSelected saat ada perubahan pada pemilihan skin tone
+  $('#skinToneSelect').on("change", function() {
+    checkSkinToneSelected();
+  });
+
+  // Memeriksa status pemilihan skin tone saat halaman dimuat
+  checkSkinToneSelected();
+
+  // Fungsi untuk memanggil API ketika tombol prediksi ditekan
+  $("#prediksi_submit").click(function(e) { 
+    e.preventDefault();
+
+    // Cek kembali apakah ada skin tone yang dipilih
+    var selectedValue = $('#skinToneSelect').val();
+    if (!selectedValue) {
+      alert("Harap pilih skin tone terlebih dahulu!");
+      return;
+    }
+
+  });
+
     // -[Prediksi Model]---------------------------
     
     // Fungsi untuk memanggil API ketika tombol prediksi ditekan
@@ -26,9 +80,9 @@ $(document).ready(function(){
                       res_gambar_prediksi = res['gambar_prediksi']
                       
                       // Tampilkan hasil prediksi ke halaman web
-                      console.log(res_data_prediksi)
-                      console.log(res_gambar_prediksi)
-                      generate_prediksi(res_data_prediksi, res_gambar_prediksi); 
+                      generate_prediksi(res_data_prediksi, res_gambar_prediksi);
+                      // Rekomennya masih belum
+                      generate_recomenn(res_data_prediksi, res_gambar_prediksi)
                 }
               });
           }
@@ -47,15 +101,27 @@ $(document).ready(function(){
       if(image_prediksi == "(none)") {
         str += "<h3>Your Image is error</h3>";
         str += "<img src='https://dummyimage.com/300x300/000/fff' alt='Gambar Produk'>";
-        str += "<p>Your Product is error</p>"
       }
       else {
-        str += "<p>Your Problem on Face: <b>"+ data_prediksi +"</b></p>";
+        str += "<p>Your Problem on Face: <b>"+ data_prediksi +"</b></p>" 
         str += "<img src='" + image_prediksi + "'width=\"350\" height=\"350\" alt='Gambar Produk'>";
-        str += "<p>Product Name: Garnier </p>" //Masih belum predict 
       }
-      $("#outputArea").html(str);
+      $("#outputAreaFace").html(str);
+    }
+
+    // Fungsi untuk menampilkan hasil prediksi rekomendasi
+    function generate_recomenn(product_prediction, product_image) {
+      var str="";
+      
+      if(image_prediksi == "(none)") {
+        str += "<h3>Your Image is error</h3>";
+        str += "<img src='https://dummyimage.com/300x300/000/fff' alt='Gambar Produk'>";
+      }
+      else {
+        str += "<p>Product on Your Face: <b>"+ product_prediction +"</b></p>" 
+        str += "<img src='" + product_image + "'width=\"350\" height=\"350\" alt='Gambar Produk'>";
+      }
+      $("#outputAreaFace").html(str);
     }  
   })
-    
   
